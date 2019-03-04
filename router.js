d0b75ed79e8c8d2db1855fb7377f446b739d0de7
carloferrer <thecarloferrer@gmail.com>
2019-03-03 19:25:05 -0500

    Implement rudimentary 'create' operation
    I'm not even sure if this works lol. Fingers crossed.

--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 import { Router as ExpressRouter} from 'express';
+import * as controller from './controller';
 
 const router = ExpressRouter();
 
@@ -6,5 +7,8 @@ router.get('/', (req, res) => res.json({
 	status: 'API working',
 	message: 'Welcome to fast_tracker',
 }));
+
+router.route('/item')
+  .post(controller.create);
 
 export default router;
